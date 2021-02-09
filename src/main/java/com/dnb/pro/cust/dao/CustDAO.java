efb342fcdd7fb009cfbe57cab4c4610d3560b168
digital10 <digital10@digital10>
2021-02-09 10:22:26 +0900

    lee_0209 10:22

--- src/main/java/com/dnb/pro/cust/dao/CustDAO.java
+++ src/main/java/com/dnb/pro/cust/dao/CustDAO.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.dao.DataAccessException;
 
+import com.dnb.pro.board.vo.ArticleVO;
+
 public interface CustDAO {
 	public List selectAllFaqList() throws DataAccessException;
+	public List selectAllNoticeList() throws DataAccessException;
+	public ArticleVO selectNoticeArticle(int  board_num) throws DataAccessException;
 }
